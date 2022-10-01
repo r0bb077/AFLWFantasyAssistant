de41f3d41e899636fadda66e46a011a837ca4c89
Rob <robw190819@gmail.com>
2022-10-01 17:41:08 +1000

    Including a live score card at the top

--- AFLW Chrome Extension/contentScript.js
+++ AFLW Chrome Extension/contentScript.js
@@ -7,12 +7,54 @@ function getPlayerData() {
     });
 }
 
+function loopNodesFindTextNode(node) {
+    let returnNode;
+    [...node.childNodes].forEach((node) => {
+        if (node.nodeType === Node.TEXT_NODE) {
+            returnNode = node;
+            return;
+        } else {
+            returnNode = loopNodesFindTextNode(node);
+            if(returnNode){
+                return;
+            }
+        }
+    })
+
+    return returnNode;
+}
+
+function insertLiveScoreCard(liveScore) {
+    let liveScoreCard = document.querySelector(".aflwLiveScoreCard");
+    if (liveScoreCard != null) {
+        const textNode = loopNodesFindTextNode(liveScoreCard);
+        textNode.textContent = liveScore;
+        return;
+    }
+
+    const scoreCardGrid = document.querySelector("#main .grid");
+    scoreCardGrid.classList.remove("md:grid-cols-6");
+    scoreCardGrid.classList.add("md:grid-cols-7");
+
+    const firstScoreCard = scoreCardGrid.childNodes[0];
+
+    liveScoreCard = document.createElement("div");
+    scoreCardGrid.appendChild(liveScoreCard);
+    liveScoreCard.innerHTML = firstScoreCard.innerHTML;
+    liveScoreCard = document.querySelector("#main .grid").childNodes[6];
+
+    const textNode = loopNodesFindTextNode(liveScoreCard);
+    textNode.textContent = liveScore;
+    liveScoreCard.querySelector("span").innerText = "Live Score";
+    liveScoreCard.classList.add("aflwLiveScoreCard");
+}
+
 function updatePlayers(players) {
     let totalScore = 0;
     [...document.querySelectorAll(".aflwCustomCard, .px-6")].forEach((playerCardElement) => {
         let nameSplit = playerCardElement.querySelector(".px-1")?.childNodes[0]?.textContent?.trim()?.split(" ");
         if (!nameSplit) {
-            nameSplit = [...playerCardElement.childNodes].find(x=>x.nodeType === Node.TEXT_NODE)?.textContent?.trim()?.split(" ");
+            nameSplit = [...playerCardElement.childNodes].find(x => x.nodeType === Node.TEXT_NODE)?.textContent?.trim()?.split(" ");
             if (!nameSplit) return;
         }
 
@@ -30,7 +72,7 @@ function updatePlayers(players) {
         const currentScore = parseInt(scoreElement.innerText);
 
         const isCaptain = playerCardElement.querySelector(".bg-red-200") != null;
-        if(isCaptain){
+        if (isCaptain) {
             score = score * 2;
         }
 
@@ -39,7 +81,7 @@ function updatePlayers(players) {
             classToAdd = "positiveScoreUpdate";
         } else if (currentScore > score) {
             classToAdd = "negativeScoreUpdate"
-        }     
+        }
 
         if (classToAdd) {
             const onFieldContainer = playerCardElement.closest(".m-3");
@@ -62,7 +104,7 @@ function updatePlayers(players) {
         }
     });
 
-    document.querySelector("#main > div.col-span-5 > .gap-4 > div:nth-child(4) > div > dt > br").nextSibling.textContent = totalScore;
+    insertLiveScoreCard(totalScore);
 }
 
 function updateCardLayout(playerCardElement) {
@@ -103,9 +145,9 @@ function updateCardLayout(playerCardElement) {
     const playerPlayingStatusIcon = playerData.querySelector(".rounded-full");
 
     let playerStatusBorderClass = "playerUnConfirmdPlayingBorder";
-    if(playerPlayingStatusIcon.classList.contains("bg-green-500")){
+    if (playerPlayingStatusIcon.classList.contains("bg-green-500")) {
         playerStatusBorderClass = "playerPlayingBorder";
-    } else if(playerPlayingStatusIcon.classList.contains("bg-red-500")){
+    } else if (playerPlayingStatusIcon.classList.contains("bg-red-500")) {
         playerStatusBorderClass = "playerNotPlayingBorder";
     }
     playerCardElement.classList.add(playerStatusBorderClass);
@@ -113,7 +155,7 @@ function updateCardLayout(playerCardElement) {
     playerPlayingStatusIcon.remove();
 
     playerDataContainer.appendChild(playerData);
-    
+
     console.log([...playerDataContainer.querySelector(".font-light")?.childNodes]);
     const scoreElem = [...playerDataContainer.querySelector(".font-light")?.childNodes].find(child => child.textContent?.trim() === "Score");
     scoreElem?.nextSibling?.remove();
